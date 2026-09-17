def957f0d89e3f77e265bf8c8e555b9377fb888d
successeverydayww-cpu <successeverydayww@gmail.com>
2026-09-17 08:54:28 +0000

    v18: sub-5s call lane (ring fn) + owner-gated Premium AI + premium TTS + presence dots (green/red) + guardian ring lane

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* AirConnect service worker: offline shell + faster loads */
-const CACHE = 'airconnect-v17';
+const CACHE = 'airconnect-v18';
 const SHELL = ['./caller.html', './manifest.json', './icon-192.png', './icon-512.png', './qrcode.min.js'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
