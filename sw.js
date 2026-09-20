8d22917dc2159b49b7600877c1a27274893a4dc0
Skylar Agent <skylarhenry492@gmail.com>
2026-09-20 22:53:21 +0000

    v67: group-call audio hardening (sub retry, SDP lock, join retries, msid fallback), one-way video fix (answer-while-live + glare rollback), WhatsApp-style on-top reactions + floating bar, Feed/Shorts/Long split

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* AirConnect service worker: offline shell + faster loads */
-const CACHE = 'airconnect-v66';
+const CACHE = 'airconnect-v67';
 const SHELL = ['./caller.html', './caller5.html', './manifest.json', './icon-192.png', './icon-512.png', './qrcode.min.js'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
